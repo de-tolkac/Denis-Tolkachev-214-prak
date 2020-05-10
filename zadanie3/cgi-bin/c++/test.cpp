dc86502b5c4ca28008b63dadcb185b7f18a7785a
de-tolkac <de-tolkac@ya.ru>
2020-05-10 20:02:21 +0300

    small fixes

--- zadanie3/cgi-bin/c++/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main (){
-    cout << "<html><body><h1>Тестовая страница для моего веб-сервера</h1><br><p>Параметры запроса: " << getenv("QUERY_STRING") << "</p></body></html>";
-    return 0; 
-}
